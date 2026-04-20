525188727b58c4973e7ded15a6c861ce488b3c35
Navarro Walter <navarrowalterd@gmail.com>
2026-04-20 20:23:28 -0300

    Agrego archivo .c

---
+++ ArregloDeNombres.c
@@ -0,0 +1,20 @@
+/* Nota 1: La librería más común para trabajar con cadenas de caracteres es string.h. Algunas 
+de sus funciones más importantes son: 
+• strlen(<cadena>): Devuelve la longitud de la cadena sin tomar en cuenta el caracter de 
+final de cadena. 
+• strcpy(<cadena_destino>, <cadena_origen>) : Copia el contenido de 
+<cadena_origen> en <cadena_destino>. 
+• strcat(<cadena_destino>, <cadena_origen>) : Concatena el contenido de 
+<cadena_origen> al final de <>. 
+• strcmp(<cadena1>, <cadena2>) : Compara las dos cadenas y devuelve un 0 si las dos 
+cadenas son iguales, un número negativo si <cadena1> es menor que <cadena2> y un número 
+positivo (mayor que cero) si <cadena1> es mayor que <cadena2> */
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main(){
+
+return 0;
+}
